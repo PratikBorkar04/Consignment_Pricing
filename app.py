faffe45787b6a96594c0ed070156c47483d4f606
Pratik Borkar <pratikab01@gmail.com>
2023-05-02 17:06:15 +0530

    flask app created

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Consginment Price"
+    return "Consignment Price"
 
 if __name__ == "__main__":
     app.run(debug=True)
