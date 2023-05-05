69ed0b9b274de981c2669fcd8842249a9c10a622
Pratik Borkar <pratikab01@gmail.com>
2023-05-05 15:58:32 +0530

    Folder structure updated

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    try:
-        raise Exception("CUSTOM EXCEPTION")
-    except Exception as ex:
-        housing =  HousingException(ex,sys)
-        logging.info(housing.error_message)
-        logging.info("TESTING")
     return "Consignment Price Preditction"
 
 if __name__ == "__main__":
